87ac3b1b43781ccaf2996cc0b610804cda2dcc6b
Wei Xia <wei_xia@hotmail.com>
2021-04-04 15:10:36 -0700

    added expo method

--- ConsoleGit/Program.cs
+++ ConsoleGit/Program.cs
@@ -9,16 +9,24 @@ namespace ConsoleGit
             Console.WriteLine("Hello World!");
             //made comments on GitHub
         }
-        
-        int add(int a ,  int b)
-        {
-            return a+b;
-        }
-            
 
-        int sub(int a , int b)
+        int add(int a, int b)
+        {
+            return a + b;
+        }
+
+
+        int sub(int a, int b)
         {
             return a - b;
+        }
+
+        int exp(int a, int b)
+        {
+            int result = 1;
+            for (int i = 0; i < b; i++)
+                result *= a;
+            return result;
         }
     }
 }
